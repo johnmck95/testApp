2372f383348f1f93dbe7d09a00719e122a2ccec6
John <john.mic.mckinnon@gmail.com>
2024-01-18 16:38:55 -0800

    Add workout comments to UI

--- my-app/src/Components/Workout.tsx
+++ my-app/src/Components/Workout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { WorkoutWithExercisesType } from "../Types/types";
-import { Box, Heading, List } from "@chakra-ui/react";
+import { Box, Flex, Heading, List } from "@chakra-ui/react";
 import Exercise from "./Exercise";
 
 export default function Workout({
@@ -16,19 +16,19 @@ export default function Workout({
       my="1rem"
       padding={["0.25rem", "0.5rem"]}
     >
-      <Heading
-        fontSize={["sm", "md"]}
-        w="100%"
-        mx={["0.5rem", "1rem"]}
-        py="0.5rem"
-      >
-        {workoutWithExercises.date.toDate().toLocaleDateString("en-CA", {
-          weekday: "long",
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        })}
-      </Heading>
+      <Flex w="100%" alignItems="flex-end" mx={["0.5rem", "1rem"]} py="0.5rem">
+        <Heading fontSize={["sm", "md"]} mr="1rem">
+          {workoutWithExercises.date.toDate().toLocaleDateString("en-CA", {
+            weekday: "long",
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          })}
+        </Heading>
+        <Heading as="h4" fontSize="xs" color="gray.600">
+          {workoutWithExercises.comment}
+        </Heading>
+      </Flex>
       <List w="100%">
         {workoutWithExercises.exercises.map((exercise) => (
           <Exercise key={exercise.uid} exercise={exercise} />
